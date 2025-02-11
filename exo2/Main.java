51090eb155ebed26ad80c99f2a81477aaec9cde2
Marin <marin.berthier@edu.ece.fr>
2025-02-11 17:55:58 +0100

    message

---
+++ exo2/Main.java
@@ -0,0 +1,8 @@
+package exo2;
+
+public class Main {
+    public static void main (String[] args){
+        CompteBancaire compte1 = new  CompteBancaire("Marin BERTHIER", 1000);
+        compte1.deposer(200);
+    }
+}
