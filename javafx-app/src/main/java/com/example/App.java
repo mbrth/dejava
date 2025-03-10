f54603bcd6935b0c2bb7d295f3307f57fdff2294
Marin <marin.berthier@edu.ece.fr>
2025-03-10 16:45:06 +0100

    10-03

--- javafx-app/src/main/java/com/example/App.java
+++ javafx-app/src/main/java/com/example/App.java
@@ -3,25 +3,34 @@ package com.example;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
+    public void handleButtonClick(Label messageLabel) {
+        messageLabel.setText("Texte modifié ");
+    }
+    
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws Exception{
         // Créer un label
-       // Label label = new Label("Bonjour, JavaFX !");
+        //Label label = new Label("Bonjour, JavaFX !");
 
         VBox root = FXMLLoader.load(getClass().getResource("/interface.fxml"));
 
         // Créer une scène avec le label
         Scene scene = new Scene(root, 300, 200);
         scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
+        Label messageLabel = (Label) root.lookup("#messageLabel");
+        Button clickButton = (Button) root.lookup("#clickButton");
+        clickButton.setOnAction(event -> this.handleButtonClick(messageLabel));
 
+   
         // Configurer la fenêtre principale
-        primaryStage.setTitle("Ma Première Application JavaFX bouba je t'aime");
+        primaryStage.setTitle("Ma Première Application javafx");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
