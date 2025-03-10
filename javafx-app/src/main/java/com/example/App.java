8b423c7aeb71ddbb0cf769abb82dc9e188ad1f64
Marin <marin.berthier@edu.ece.fr>
2025-03-10 14:57:05 +0100

    10/03

--- javafx-app/src/main/java/com/example/App.java
+++ javafx-app/src/main/java/com/example/App.java
@@ -1,23 +1,27 @@
 package com.example;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
         // Créer un label
-        Label label = new Label("Bonjour, JavaFX !");
+       // Label label = new Label("Bonjour, JavaFX !");
+
+        VBox root = FXMLLoader.load(getClass().getResource("/interface.fxml"));
 
         // Créer une scène avec le label
-        Scene scene = new Scene(label, 300, 200);
+        Scene scene = new Scene(root, 300, 200);
         scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
 
         // Configurer la fenêtre principale
-        primaryStage.setTitle("Ma Première Application JavaFX");
+        primaryStage.setTitle("Ma Première Application JavaFX bouba je t'aime");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
